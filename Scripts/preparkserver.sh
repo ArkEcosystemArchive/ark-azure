9d03081635f42ade2655fc8b85d3f419558d69cf
davepinkawa <davepinkawa@gmail.com>
2018-02-05 16:51:51 -0600

    testing cleaned up dep list

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sudo apt-get -y update
-sleep 60
-sudo apt-get -y install jq postgresql postgresql-contrib libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev forever grunt-cli node-sass angular-cli nodemailer node-uuid
-sleep 120
+sudo apt-get -y update && sudo apt-get -y install jq postgresql postgresql-contrib libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
