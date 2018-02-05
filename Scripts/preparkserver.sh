518bda0ddf3257942b3a1e2b3ded7162cebcdea8
davepinkawa <davepinkawa@gmail.com>
2018-02-05 14:52:01 -0600

    omg

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update 
-sudo apt-get install -y jq postgresql  postgresql-contrib  libpq-dev  build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev forever grunt-cli node-sass angular-cli nodemailer node-uuid
+sudo apt-get update && sudo apt-get install -y jq postgresql  postgresql-contrib  libpq-dev  build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev forever grunt-cli node-sass angular-cli nodemailer node-uuid
