f6a72e43bc58f88fd07e89e26bdd68e40ca7d532
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-05 22:51:12 -0600

    test9

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get -y update && sudo apt-get -y install npm jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
 npm install forever@0.15.3 grunt-cli@1.2.0 node-sass@4.7.2 angular-cli@1.0.0-beta.28.3
-git clone https://github.com/ArkEcosystem/ark-deployer.git /home/arkadmin/ark-deployer && cd ark-deployer
+git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 source ~/.profile
 nvm install 8.9.1
