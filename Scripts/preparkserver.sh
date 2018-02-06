ef1fc7aa33c6573a6e080a3379232bffd2ada7c7
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-05 22:02:02 -0600

    test5

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -2,7 +2,6 @@
 sudo apt-get -y update && sudo apt-get -y install npm jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
 sudo npm install forever@0.15.3 grunt-cli@1.2.0 node-sass@4.7.2 angular-cli@1.0.0-beta.28.3
 git clone https://github.com/ArkEcosystem/ark-deployer.git /home/davidpinkawa/ark-deployer
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source ~/.profile
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | NVM_DIR=/home/davidpinkawa/.nvm bash
 nvm install 8.9.1
 sudo apt-get -y install postgresql postgresql-contrib
