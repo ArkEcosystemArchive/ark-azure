a2f069658feed519c2fcc5402bbee6e90b377d92
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-05 23:52:23 -0600

    removed unnecessary items from list

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt-get -y update && sudo apt-get -y install npm jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev nodejs
-sudo npm install forever@0.15.3 grunt-cli@1.2.0 node-sass@4.7.2 angular-cli@1.0.0-beta.28.3
-git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
+sudo apt-get -y update && sudo apt-get -y install npm jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
 sudo apt-get -y install postgresql postgresql-contrib
