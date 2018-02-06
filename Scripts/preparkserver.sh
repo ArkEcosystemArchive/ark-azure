1af3796a7d22b84b0e51aacdd0b911457df94bd5
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-05 18:39:56 -0600

    ark-installs

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 sudo apt-get -y update && sudo apt-get -y install jq postgresql postgresql-contrib libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
+git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.profile
+nvm install 8.9.1
+./sidechain.sh install-node --name MyTest --database ark_mytest --token MYTEST --symbol MT
