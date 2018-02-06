d4f09a693293d8faf7fa7676d4343110fd4a251d
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-05 20:38:50 -0600

    adding experimental install steps

--- Scripts/preparkserver.sh
+++ Scripts/preparkserver.sh
@@ -2,4 +2,8 @@
 sudo apt-get -y update && sudo apt-get -y install jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
 sudo npm install forever@0.15.3 grunt-cli@1.2.0 node-sass@4.7.2 angular-cli@1.0.0-beta.28.3
 sudo apt-get -y install postgresql postgresql-contrib
+git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.profile
+nvm install 8.9.1
 
