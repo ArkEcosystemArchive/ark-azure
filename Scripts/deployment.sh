0d2b48f0d7bd0be6bdae149ac751ca555f1c5c12
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-06 19:42:30 -0600

    nvm made manual only

--- Scripts/deployment.sh
+++ Scripts/deployment.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-git clone https://github.com/ArkEcosystem/ark-deployer.git && cd ark-deployer
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source ~/.profile
-nvm install 8.9.1
 
 # Variables
 SIDECHAINNAME=MyTest
