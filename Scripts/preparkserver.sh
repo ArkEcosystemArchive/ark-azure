5f9a2b33de9e5864a1f8446d10a6eec7d9b35248
davepinkawa <davepinkawa@users.noreply.github.com>
2018-02-16 22:09:40 -0600

    Working Ark to Azure
    Not dependent on Azure approving pull request

--- Scripts/preparkserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt-get -y update && sudo apt-get -y install npm jq libpq-dev build-essential libtool autoconf automake zip unzip htop nmon iftop pkg-config libcairo2-dev libgif-dev
-sudo apt-get -y install postgresql postgresql-contrib
